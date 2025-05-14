8f79520bc73b0323d56ad0ec777324559c625a8a
Emile <emile@drhp223e.windows.dauphine.fr>
2025-05-14 12:53:27 +0200

    Budget preprocessing

--- src/movies_preprocessing.py
+++ src/movies_preprocessing.py
@@ -1,5 +1,6 @@
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, MultiLabelBinarizer
+from sklearn.preprocessing import StandardScaler, MultiLabelBinarizer
 import pandas as pd
+import numpy as np
 
 def genre_encoding(df):
     df["genres_list"] = df["genres"].str.split(",")
@@ -9,3 +10,16 @@ def genre_encoding(df):
     df_encoded = pd.concat([df.drop(columns=["genres", "genres_list"]), genre_df], axis=1)
     return df_encoded
 
+def budget_processing(df, replacement_value):
+    df['budget_filled'] = df['budget']
+    if replacement_value == 'mean':
+        df['budget_filled'] = df.groupby('year_film')['budget_filled'].transform(lambda x: x.fillna(x.mean()))
+    else:
+        df['budget_filled'] = df.groupby('year_film')['budget_filled'].transform(lambda x: x.fillna(x.median()))
+    df['budget_log'] = np.log1p(df['budget_filled'])
+    df.drop(columns=["budget", "budget_filled"])
+    return df
+
+df = pd.read_csv('../data/final_data.csv')
+budget = budget_processing(df, "mean")
+budget.to_csv("logbudget.csv", index = False)
