71a8e02d91af0d05e06b4ee0913847646d7a95f4
Emile <emile@drhp223e.windows.dauphine.fr>
2025-05-14 12:40:15 +0200

    Genres encoding

--- src/movies_preprocessing.py
+++ src/movies_preprocessing.py
@@ -2,14 +2,10 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler, MultiLabelBinar
 import pandas as pd
 
 def genre_encoding(df):
+    df["genres_list"] = df["genres"].str.split(",")
     mlb = MultiLabelBinarizer()
-    encoded_genres = mlb.fit_transform(df["genres"])
+    encoded_genres = mlb.fit_transform(df["genres_list"])
     genre_df = pd.DataFrame(encoded_genres, columns=mlb.classes_, index=df.index)
-    df_encoded = pd.concat([df.drop(columns=["genres"]), genre_df], axis=1)
+    df_encoded = pd.concat([df.drop(columns=["genres", "genres_list"]), genre_df], axis=1)
     return df_encoded
 
-
-df = pd.read_csv('../data/final_data.csv')
-encoded = genre_encoding(df)
-encoded.to_csv('encoded.csv', index=False)
-print(encoded)
